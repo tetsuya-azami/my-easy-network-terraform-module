26644df4dc8ce8761f1d46cfd8e2eb5e1a53e588
tetsuya-azami <zqianjian35@gmail.com>
2024-01-25 08:40:34 +0900

    fix map to set

--- modules/my-easy-network-terraform-module/output.tf
+++ modules/my-easy-network-terraform-module/output.tf
@@ -3,9 +3,9 @@ output "vpc_id" {
 }
 
 output "public_subnet_ids" {
-  value = { for k, v in aws_subnet.public : k => v.id }
+  value = [for k, v in aws_subnet.public : v.id]
 }
 
 output "private_subnet_ids" {
-  value = { for k, v in aws_subnet.private : k => v.id }
+  value = [for k, v in aws_subnet.private : v.id]
 }
