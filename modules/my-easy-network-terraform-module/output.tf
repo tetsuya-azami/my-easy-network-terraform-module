52725d69a89888db03c953eabaa9edaa1c638825
tetsuya-azami <zqianjian35@gmail.com>
2024-01-25 07:42:17 +0900

    add network module outputs

---
+++ modules/my-easy-network-terraform-module/output.tf
@@ -0,0 +1,11 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = { for k, v in aws_subnet.public : k => v.id }
+}
+
+output "private_subnet_ids" {
+  value = { for k, v in aws_subnet.private : k => v.id }
+}
